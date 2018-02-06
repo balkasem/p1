9a16e178e9f93da4d5e784851f34dee5b253df99
Sam Balkasem <obalkasem@gmail.com>
2018-02-06 01:38:30 -0500

    New Changes

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <!doctype html>
 <html>
 <head>
-
+	<link href='../main.css' rel='stylesheet' > 
 	<title>Osama Balkasem</title>
 	<meta charset='utf-8'>
-		
 </head>
-	<h3> Osama Balkasem </h3>
-	<br/>
-	<img alt='MyPicture' src='../me.jpg'>
-	<br/>
-	<h3> About me </h3>
-	<p> 
-		I'm Osama Balkasem, software engineer. Currently taking courses in Harvard Extesnion School to pursue a degree in Software Engineering. <br/> I like to play soccor and tennis. I do vocal performance for traditional Syrian music.
-	</p>
-	<br/>
-	<h3> Random Quote </h3>
-	<?php
-$temperature = 75;
-$scale = 'F';
-$location = "Cambridge";
-
-echo 'Temperature in '.$location.': '.$temperature.' '.$scale;
-?>
+	<div id="container" >
+			<h1> Osama Balkasem </h1>
+			<br/>
+				<img alt='Osama Balkasem' src='../me.jpg'>
+			<br/>
+				<h2> About me </h2>
+				<p> 
+					I'm Osama Balkasem, I work professionally as a software engineer in Pets Supplies Plus, Livonia Michigan. Currently taking courses in Harvard Extesnion School to pursue a degree in Software Engineering. 
+					In my free time I enjoy reading, playing soccor and tennis. Also, I do vocal performance for traditional Syrian music.
+				</p>
+			<br/>
+				<h2> Random Quote </h2>
+				<?php
+					$quotes = [
+						0 => '“Be yourself; everyone else is already taken.” ― Oscar Wilde',
+						1 => '“Be the change that you wish to see in the world.” ― Mahatma Gandhi',
+						2 => '“In three words I can sum up everything Ive learned about life: it goes on.” ― Robert Frost'
+					];
+					echo "<p>" . $quotes[rand(0, 2)] . "</p>"  . "<br/>" ;
+				?>
+	</div>
 </html>
