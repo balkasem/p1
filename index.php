190f927d1a8fec7dbfbadda25a7aa237df8b3658
Sam Balkasem <obalkasem@gmail.com>
2018-02-06 00:35:33 -0500

    New Ghanges

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+
+	<title>Osama Balkasem</title>
+	<meta charset='utf-8'>
+		
+</head>
+	<h3> Osama Balkasem </h3>
+	<br/>
+	<img alt='MyPicture' src='../me.jpg'>
+	<br/>
+	<h3> About me </h3>
+	<p> 
+		I'm Osama Balkasem, software engineer. Currently taking courses in Harvard Extesnion School to pursue a degree in Software Engineering. <br/> I like to play soccor and tennis. I do vocal performance for traditional Syrian music.
+	</p>
+	<br/>
+	<h3> Random Quote </h3>
+	<?php
+$temperature = 75;
+$scale = 'F';
+$location = "Cambridge";
+
+echo 'Temperature in '.$location.': '.$temperature.' '.$scale;
+?>
+</html>
