b81737a4d88a33b7cfc4a2c8496c9ec2ff3a44d7
Sam Balkasem <obalkasem@gmail.com>
2018-02-07 20:14:32 -0500

    Modified style and add two new folders for images and styles

--- index.php
+++ index.php
@@ -1,29 +1,31 @@
 <!doctype html>
 <html>
 <head>
-	<link href='../main.css' rel='stylesheet' > 
+	<link href='../css/main.css' rel='stylesheet' > 
 	<title>Osama Balkasem</title>
 	<meta charset='utf-8'>
+	
 </head>
-	<div id="container" >
-			<h1> Osama Balkasem </h1>
-			<br/>
-				<img alt='Osama Balkasem' src='../me.jpg'>
-			<br/>
-				<h2> About me </h2>
-				<p> 
-					I'm Osama Balkasem, I work professionally as a software engineer in Pets Supplies Plus, Livonia Michigan. Currently taking courses in Harvard Extesnion School to pursue a degree in Software Engineering. 
-					In my free time I enjoy reading, playing soccor and tennis. Also, I do vocal singing for traditional Syrian music.
-				</p>
-			<br/>
-				<h2> Random Quote </h2>
-				<?php
-					$quotes = [
-						0 => '“Be yourself; everyone else is already taken.” ― Oscar Wilde',
-						1 => '“Be the change that you wish to see in the world.” ― Mahatma Gandhi',
-						2 => '“In three words I can sum up everything Ive learned about life: it goes on.” ― Robert Frost'
-					];
-					echo "<p>" . $quotes[rand(0, 2)] . "</p>"  . "<br/>" ;
-				?>
-	</div>
+		<body>
+			<div class="tainer" >
+				<h1> Osama Balkasem </h1>
+					<img alt='Osama Balkasem' src='../images/me.jpg'>
+				<br/>
+					<h2> About me </h2>
+					<p> 
+						I'm Osama Balkasem, I work professionally as a software engineer in Pets Supplies Plus, Livonia Michigan. Currently taking courses in Harvard Extesnion School to pursue a degree in Software Engineering. 
+						In my free time I enjoy reading, playing soccor and tennis. Also, I do vocal singing for traditional Syrian music.
+					</p>
+				<br/>
+					<h2> Random Quotes </h2>
+					<?php
+						$quotes = [
+							0 => '“Be yourself; everyone else is already taken.” ― Oscar Wilde',
+							1 => '“Be the change that you wish to see in the world.” ― Mahatma Gandhi',
+							2 => '“In three words I can sum up everything Ive learned about life: it goes on.” ― Robert Frost'
+						];
+						echo "<p>" . $quotes[rand(0, 2)] . "</p>"  . "<br/>" ;
+					?>
+			</div>	
+		</body>
 </html>
